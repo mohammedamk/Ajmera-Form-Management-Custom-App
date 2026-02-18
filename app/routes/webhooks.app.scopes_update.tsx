7105e5102342b3010a044911bc7b235137a1fb49
mohammedamk <mohammedamk1@gmail.com>
2026-02-18 15:25:46 +0530

    fixed build issue

--- app/routes/webhooks.app.scopes_update.tsx
+++ app/routes/webhooks.app.scopes_update.tsx
@@ -1,6 +1,6 @@
 import type { ActionFunctionArgs } from "react-router";
 import { authenticate } from "../shopify.server";
-import db from "../db.server";
+
 
 export const action = async ({ request }: ActionFunctionArgs) => {
     const { payload, session, topic, shop } = await authenticate.webhook(request);
@@ -8,14 +8,7 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 
     const current = payload.current as string[];
     if (session) {
-        await db.session.update({   
-            where: {
-                id: session.id
-            },
-            data: {
-                scope: current.toString(),
-            },
-        });
+       
     }
     return new Response();
 };
